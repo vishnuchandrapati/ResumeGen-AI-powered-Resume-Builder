0163fd101cf7dc7850f5513e62b140d26f65a81f
Vishnu <vishnuchandrapati2003@gmail.com>
2025-12-26 21:55:04 +0530

    Modified Home Controller which is responsible for viewing the dashboard of the user

--- resumeGen/src/main/java/com/vishnu/resumeGen/controller/HomeController.java
+++ resumeGen/src/main/java/com/vishnu/resumeGen/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.vishnu.resumeGen.controller;
 
 import com.vishnu.resumeGen.model.UserDetails;
+import com.vishnu.resumeGen.repository.UserRepo;
 import com.vishnu.resumeGen.service.UserDetailsService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -11,15 +12,20 @@ import org.springframework.web.bind.annotation.*;
 public class HomeController {
     @Value("${spring.application.name}")
     private String appName;
-
+    private UserRepo userRepo;
     private UserDetailsService userDetailsService;
 
-    public HomeController(UserDetailsService userDetailsService){
+    public HomeController(UserRepo userRepo, UserDetailsService userDetailsService){
+        this.userRepo = userRepo;
         this.userDetailsService = userDetailsService;
     }
-    @PostMapping("/")
-    public UserDetails userDetailsService(@RequestBody UserDetails userDetails){
-        return userDetailsService.saveUser(userDetails);
+    @GetMapping("/")
+//    public UserDetails userDetailsService(@RequestBody UserDetails userDetails){
+//        return userDetailsService.saveUser(userDetails);
+//    }
+    public UserDetails getAllUserDetails(@RequestParam String mail){
+
+        return userRepo.findByUserMail(mail);
     }
 
 }
